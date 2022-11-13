8a4213e33e82e4c20c50f17d3f1b5e035ab50304
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-11-13 23:16:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { App } from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import  store  from 'redux/store';
 //import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
